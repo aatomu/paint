3c353f1c342302991652b7e88eef7b23620af78b
aatomu <aatomu21263+git@gmail.com>
2025-09-13 00:51:38 +0900

    ✨ roomに飛べるように

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+//@ts-check
+
+/** @type {HTMLInputElement} */
+// @ts-expect-error
+const input_name = document.getElementById("input_name")
+/** @type {HTMLInputElement} */
+// @ts-expect-error
+const input_room = document.getElementById("input_room")
+/** @type {HTMLButtonElement} */
+// @ts-expect-error
+const input_join = document.getElementById("input_join")
+
+
+input_join.addEventListener("click", () => {
+  if (input_name.value !== "" && input_room.value !== "") {
+    document.cookie = `name=${input_name.value}`
+    const url = new URL(window.location.href)
+    url.pathname = "room"
+    url.search = ""
+    url.searchParams.append("id", input_room.value)
+    window.location.href = url.toString()
+  }
+})
