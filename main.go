050a1372c262947f414423fd002df98ca15b2091
aatomu <aatomu21263+git@gmail.com>
2025-09-13 00:27:25 +0900

    🎨 .htmlをつけなくても開けるように

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"path"
 	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/aatomu/atomicgo/files"
 	"golang.org/x/net/websocket"
@@ -14,6 +16,33 @@ var (
 	Listen = ":1026"
 )
 
+type fallbackFileSystem struct {
+	fs http.FileSystem
+}
+
+func (ff fallbackFileSystem) Open(name string) (http.File, error) {
+	f, err := ff.fs.Open(name)
+	if err == nil {
+		return f, nil
+	}
+
+	// ファイルが見つからない場合は .html を付けて再試行
+	if !strings.HasSuffix(name, ".html") {
+		if f2, err2 := ff.fs.Open(name + ".html"); err2 == nil {
+			return f2, nil
+		}
+	}
+
+	// ディレクトリなら index.html を見る
+	if strings.HasSuffix(name, "/") {
+		if f3, err3 := ff.fs.Open(path.Join(name, "index.html")); err3 == nil {
+			return f3, nil
+		}
+	}
+
+	return nil, err
+}
+
 func main() {
 	// Work dir
 	_, file, _, _ := runtime.Caller(0)
@@ -21,7 +50,7 @@ func main() {
 	files.SetWorkDir(goDir)
 
 	// Http handle
-	assets := http.FileServer(http.Dir("./assets"))
+	assets := http.FileServer(fallbackFileSystem{http.Dir("./assets")})
 	http.Handle("/", middleware(assets))
 	http.Handle("/ws", middleware(websocket.Handler(WebsocketRequest)))
 
