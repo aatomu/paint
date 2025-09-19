bc3912cf421309320752a01acd39a791934e889d
aatomu <aatomu21263+git@gmail.com>
2025-09-19 23:55:12 +0900

    🎨 Struct/Database Tableの整備

--- struct.go
+++ struct.go
@@ -2,134 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"path"
 	"strings"
 	"sync"
+	"time"
 
+	"github.com/google/uuid"
 	"golang.org/x/net/websocket"
 )
 
-type Room struct {
-	sync.RWMutex
-	Conn map[int64]*websocket.Conn
-}
-
-type Event struct {
-	EventId   int64
-	Timestamp int64 // UNIX timestamp
-	User      string
-	Operation string
-}
-
-type EventObjectAdd struct {
-	EventId int64
-	Id      string
-	Bold    float64
-	Color   string
-	Opacity float64
-	Type    string
-}
-
-type ObjectPen struct {
-	Id string
-	D  string
-}
-type ObjectLine struct {
-	Id     string
-	StartX float64
-	StartY float64
-	EndX   float64
-	EndY   float64
-}
-type ObjectStamp struct {
-	Id   string
-	PosX float64
-	PosY float64
-	Text string // ["aaaa","bbb", ...]
-}
-
-// 削除操作
-type EventObjectRemove struct {
-	EventId int64
-	Target  string // TypeScriptのstringに対応
-}
-
-func CreateTables(db *sql.DB) error {
-	_, err := db.Exec(`
-	CREATE TABLE IF NOT EXISTS event (
-		event_id INTEGER PRIMARY KEY,
-		timestamp TEXT NOT NULL,
-		user TEXT NOT NULL,
-		operation TEXT NOT NULL
-	)`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS event_object_add (
-		event_id INTEGER PRIMARY KEY,
-		id TEXT NOT NULL,
-		bold REAL NOT NULL,
-		color TEXT NOT NULL,
-		opacity REAL NOT NULL,
-		type TEXT NOT NULL,
-		FOREIGN KEY(event_id) REFERENCES event(event_id)
-	)`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS object_pen (
-		id TEXT PRIMARY KEY,
-		d TEXT NOT NULL,
-		FOREIGN KEY(id) REFERENCES event_object_add(id)
-	)`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS object_line (
-		id TEXT PRIMARY KEY,
-		start_x REAL NOT NULL,
-		start_y REAL NOT NULL,
-		end_x REAL NOT NULL,
-		end_y REAL NOT NULL,
-		FOREIGN KEY(id) REFERENCES event_object_add(id)
-	)`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS object_stamp (
-		id TEXT PRIMARY KEY,
-		pos_x REAL NOT NULL,
-		pos_y REAL NOT NULL,
-		text TEXT NOT NULL,
-		FOREIGN KEY(id) REFERENCES event_object_add(id)
-	)`)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS event_object_remove (
-		event_id INTEGER PRIMARY KEY,
-		target TEXT NOT NULL,
-		FOREIGN KEY(event_id) REFERENCES event(event_id)
-	)`)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
+// MARK: HTTP
 type fallbackFileSystem struct {
 	fs http.FileSystem
 }
@@ -157,6 +43,117 @@ func (ff fallbackFileSystem) Open(name string) (http.File, error) {
 	return nil, err
 }
 
+type Room struct {
+	sync.RWMutex
+	Conn map[int64]*websocket.Conn
+}
+
+// MARK: Packet
+type PacketEvent struct {
+	Id string `json:"id"`
+	// c=>s :"mouse"|"create"|"delete"|"undo"|"redo"|"clear"
+	// s=>c :"success"|"error"|"transfer"
+	Operation string `json:"operation"`
+	Data      string `json:"data"`
+}
+
+type PacketEventMouse struct {
+	Pos [2]float64 `json:"pos"`
+}
+
+type PacketEventCreate struct {
+	Id       string  `json:"id"`
+	Type     string  `json:"type"` // "pen"|"line"|"stamp"
+	Bold     float64 `json:"bold"`
+	Color    string  `json:"color"`
+	Opacity  float64 `json:"opacity"`
+	Property string  `json:"property"`
+}
+
+type PacketEventDelete struct {
+	Target string `json:"target"`
+}
+
+type PacketEventUndo struct {
+	Target string `json:"target"`
+}
+
+type PacketEventRedo struct {
+	Target string `json:"target"`
+}
+
+// MARK: Generic
+type PropertyPen struct {
+	D string `json:"d"`
+}
+type PropertyLine struct {
+	Start [2]float64 `json:"start"`
+	End   [2]float64 `json:"end"`
+}
+type PropertyStamp struct {
+	Pos  [2]float64 `json:"pos"`
+	Text string     `json:"text"`
+}
+
+// MARK: SQL commands
+func CreateTables(db *sql.DB) error {
+	_, err := db.Exec(`
+	CREATE TABLE IF NOT EXISTS boards (
+		id               TEXT    PRIMARY KEY,
+		name             TEXT    NOT NULL,
+		create_timestamp INTEGER NOT NULL
+	)`)
+	if err != nil {
+		return fmt.Errorf("%s(in boards)", err.Error())
+	}
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS elements (
+		id       TEXT    NOT NULL,
+		board_id TEXT    NOT NULL,
+		type     TEXT    NOT NULL,
+		bold     REAL    NOT NULL,
+		color    TEXT    NOT NULL,
+		opacity  REAL    NOT NULL,
+		property TEXT    NOT NULL,
+		deleted  INTEGER NOT NULL,
+		PRIMARY KEY (id,board_id),
+		FOREIGN KEY(board_id) REFERENCES boards(id)
+	)`)
+	if err != nil {
+		return fmt.Errorf("%s(in elements)", err.Error())
+	}
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS events (
+		id         TEXT    PRIMARY KEY,
+		board_id   TEXT    NOT NULL,
+		element_id TEXT,
+		username   TEXT    NOT NULL,
+		operation  TEXT    NOT NULL,
+		timestamp  INTEGER NOT NULL,
+		FOREIGN KEY(board_id) REFERENCES boards(id),
+		FOREIGN KEY(element_id) REFERENCES elements(id)
+	)`)
+	if err != nil {
+		return fmt.Errorf("%s(in events)", err.Error())
+	}
+
+	return nil
+}
+
+func GetBoardId(name string) (id string, err error) {
+	err = DB.QueryRow("SELECT id FROM boards WHERE name = ?", name).Scan(&id)
+	if err == sql.ErrNoRows {
+		id = uuid.New().String()
+		now := time.Now().Unix()
+		_, err = DB.Exec("INSERT INTO boards (id, name, create_timestamp) VALUES (?, ?, ?)", id, name, now)
+		return
+	}
+	return
+}
+
+// MARK: Websocket
 type byteReader struct {
 	io.Reader
 }
@@ -167,4 +164,10 @@ func (br *byteReader) ReadByte() (byte, error) {
 		return 0, err
 	}
 	return b[0], nil
+}
+
+func NewDecoder(t string) (d *json.Decoder) {
+	d = json.NewDecoder(strings.NewReader(t))
+	d.DisallowUnknownFields()
+	return
 }
